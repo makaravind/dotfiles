a2602ab5faa1781ef8be0ec21c76ef5592c3ddf6
Aravind M <makaravind11195@gmail.com>
2020-06-16 22:11:22 +0530

    Update .term-timeout.sh

--- .term-timeout.sh
+++ .term-timeout.sh
@@ -2,6 +2,8 @@
 
 # https://www.npmjs.com/package/console-countdown
 # Place custom sounds in path ~/Library/Sounds (Mac)
+# Cowsay - https://en.wikipedia.org/wiki/Cowsay - brew install cowsay (Homebrew)
+# https://code-maven.com/display-notification-from-the-mac-command-line
 function timer() {
    countdown -e -c $1 && osascript -e 'display notification "Done" with title "Term Timeout" sound name "Uhoh"' && clear && cowsay -f ren 'Done!'
 }
