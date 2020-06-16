c8794ecd3b611a40cb6dc7922b2ecbbc415f88fc
Aravind M <makaravind11195@gmail.com>
2020-06-16 22:09:26 +0530

    Update .term-timeout.sh

--- .term-timeout.sh
+++ .term-timeout.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # https://www.npmjs.com/package/console-countdown
+# Place custom sounds in path ~/Library/Sounds (Mac)
 function timer() {
    countdown -e -c $1 && osascript -e 'display notification "Done" with title "Term Timeout" sound name "Uhoh"' && clear && cowsay -f ren 'Done!'
 }
